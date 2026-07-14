106c5c67b9998be1a4a9c4eed9475cc33c05ff79
Huzaifa <245169@aack.au.edu.pk>
2026-07-14 12:48:41 +0500

    Update throttledCleanup.js

--- src/utils/throttledCleanup.js
+++ src/utils/throttledCleanup.js
@@ -1,7 +1,14 @@
-const { cleanupExpiredRides, cleanupExpiredCustomerRequests } = require('./lifecycleCleanup');
+const { 
+  cleanupExpiredRides, 
+  cleanupExpiredCustomerRequests,
+  autoArchiveConfirmedDeals,
+  cleanupOldCompletedDeals
+} = require('./lifecycleCleanup');
 
 let lastRideCleanup = 0;
 let lastRequestCleanup = 0;
+let lastArchiveCleanup = 0;
+let lastDeleteCleanup = 0;
 const CLEANUP_INTERVAL_MS = 60 * 1000;
 
 async function maybeCleanupExpiredRides() {
@@ -18,4 +25,23 @@ async function maybeCleanupExpiredCustomerRequests() {
   await cleanupExpiredCustomerRequests();
 }
 
-module.exports = { maybeCleanupExpiredRides, maybeCleanupExpiredCustomerRequests };
+async function maybeAutoArchiveConfirmedDeals() {
+  const now = Date.now();
+  if (now - lastArchiveCleanup < CLEANUP_INTERVAL_MS) return;
+  lastArchiveCleanup = now;
+  await autoArchiveConfirmedDeals();
+}
+
+async function maybeCleanupOldCompletedDeals() {
+  const now = Date.now();
+  if (now - lastDeleteCleanup < CLEANUP_INTERVAL_MS) return;
+  lastDeleteCleanup = now;
+  await cleanupOldCompletedDeals();
+}
+
+module.exports = { 
+  maybeCleanupExpiredRides, 
+  maybeCleanupExpiredCustomerRequests,
+  maybeAutoArchiveConfirmedDeals,
+  maybeCleanupOldCompletedDeals
+};
